2562c73019555541c3efd0cc1131a9191bc31bd5
wankong <1549072621@qq.com>
2025-07-08 11:37:56 +0800

    feat: migrate to ES modules

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,8 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(new URL(import.meta.url));
+const __dirname = path.dirname(__filename)
 
 const resolve = (filePath: string) => path.resolve(__dirname, filePath)
 
